f7b88fc89c49169ee775bf962c97f18d4f49aa28
Illia <illia.horban@gmail.com>
2024-03-05 11:03:45 +0200

    fix post method second

--- app/api/return/route.ts
+++ app/api/return/route.ts
@@ -1,20 +1,6 @@
-// app/api/return/route.ts или pages/api/return/route.ts (убедитесь, что путь правильный)
+export async function POST(req: any, res: any) {
+  const eventData = req.body;
+  console.log("Received webhook event:", eventData);
 
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    // Обработка POST запроса
-    const eventData = req.body;
-    console.log("Received webhook event:", eventData);
-
-    res.status(200).json({ message: "Event received" });
-  } else {
-    // Обработка всех остальных методов
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method Not Allowed`);
-  }
+  res.status(200).json({ message: "Event received" });
 }
