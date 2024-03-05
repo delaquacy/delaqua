ffa233e4464dcc672fc05b3f09fa3f2ad305cf82
Illia <illia.horban@gmail.com>
2024-03-05 13:37:24 +0200

    fix types

--- app/api/return/route.ts
+++ app/api/return/route.ts
@@ -1,10 +1,16 @@
-export async function POST(req: any, res: any) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export async function POST(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
   try {
-    const eventData = await req.json();
+    const eventData = req.body;
 
     console.log("Received webhook event:", eventData);
     res.status(200).send("Данные получены");
   } catch (error) {
     console.log("Ошибка при обработке запроса:", error);
+    res.status(500).send("Ошибка сервера");
   }
 }
