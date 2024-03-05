919ad823588a616c3f618c84c6ed9d316a29c3a1
Illia <illia.horban@gmail.com>
2024-03-05 11:37:15 +0200

    remove middleware

--- middleware.js
+++
@@ -1,5 +0,0 @@
-// middleware.js
-export function middleware(request) {
-  console.log("Request:", request.nextUrl.pathname);
-  // Вы можете добавить логику для изменения ответа или запроса здесь
-}
