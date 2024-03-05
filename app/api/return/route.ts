152f51475356d55340ac32b10c0c1e8b81aed1c9
Illia <illia.horban@gmail.com>
2024-03-05 11:55:32 +0200

    add answear to revolut

--- app/api/return/route.ts
+++ app/api/return/route.ts
@@ -3,8 +3,7 @@ export async function POST(req: any, res: any) {
     const eventData = await req.json();
 
     console.log("Received webhook event:", eventData);
-
-    res.status(200).json({ message: "Event received" });
+    res.status(200).send("Данные получены");
   } catch (error) {
     console.log("Ошибка при обработке запроса:", error);
   }
