acd0e7793c09dc23df8ad5dd69b97031c8accd6d
Illia <illia.horban@gmail.com>
2024-03-05 13:23:20 +0200

    return old version

--- app/api/return/route.ts
+++ app/api/return/route.ts
@@ -1,16 +1,10 @@
 export async function POST(req: any, res: any) {
   try {
-    // Предполагается, что тело запроса уже распарсено Next.js автоматически
-    const eventData = req.body;
+    const eventData = await req.json();
 
     console.log("Received webhook event:", eventData);
-    // Используйте методы Next.js для отправки статуса и ответа
-    return res.status(200).json({ message: "Данные получены" });
+    res.status(200).send("Данные получены");
   } catch (error) {
     console.log("Ошибка при обработке запроса:", error);
-    // Обязательно отправляйте HTTP-ответ в блоке catch, чтобы избежать ошибок о том, что ответ не был возвращен
-    return res
-      .status(500)
-      .json({ error: "Внутренняя ошибка сервера" });
   }
 }
