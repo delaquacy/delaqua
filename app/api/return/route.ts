b5177fd63eca15aa5863d819c58bec376d70c771
Illia <illia.horban@gmail.com>
2024-03-06 15:14:38 +0200

    fix files

--- app/api/return/route.ts
+++ app/api/return/route.ts
@@ -1,5 +1,9 @@
+import axios from "axios";
 import type { NextApiResponse } from "next";
 import { NextRequest } from "next/server";
+
+const link =
+  "https://script.google.com/macros/s/AKfycbwRabcxWmrjWwPDbByx7otDl_DyXq2CcyZeX77MWyBzljDtUm9WLRtuNkCYshPP42gD/exec";
 
 export async function POST(
   req: NextRequest,
@@ -9,6 +13,15 @@ export async function POST(
     const eventData = await req.json();
 
     console.log("Received webhook event:", eventData);
+    const postData = {
+      orderId: eventData.order_id,
+      orderData: eventData.events,
+    };
+    await axios.post(link, postData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     res.status(200).send("Данные получены");
   } catch (error) {
     console.log("Ошибка при обработке запроса:", error);
