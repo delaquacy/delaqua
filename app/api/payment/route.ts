074e546af3e77de7bc7336606892b4f18c096827
Illia <illia.horban@gmail.com>
2024-03-30 13:15:41 +0200

    google analytics test

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -2,11 +2,10 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 const key =
   "sk_CAWFozjx49HzfPb05oju56ciYeaVc28IG8-fAgNO3oE_KyvdPGaSW3ysNZIDiCVB";
+
 export async function POST(req: any, res: any) {
   const body = await req.json();
   const { currency, amount, description } = body;
-
-  console.log(amount, currency, description);
   try {
     const response = await axios.post(
       "https://sandbox-merchant.revolut.com/api/orders",
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./ui/themeMui";
 import Header from "./components/header/page";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "DelAqua",
@@ -19,6 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y4KS00EC7H"
+        />
+
+        <Script id="google-analytics">
+          {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-Y4KS00EC7H');
+          `}
+        </Script>
         <link rel="icon" href="/favicon.png" />
       </head>
       <body className={`${inter.className} antialiased`}>
