a9fcfe110906cefedc34e6a49fd9354112fab771
Illia <illia.horban@gmail.com>
2024-04-10 15:24:02 +0300

    remove folders

--- app/utils/webhoooks.ts
+++ app/utils/webhoooks.ts
@@ -1,59 +1,11 @@
 import axios from "axios";
-
-// export const requestToReturnSuccessStatus = async () => {
-//   const webhookUrl = "https://delaqua.vercel.app/api/return";
-//   const events = ["ORDER_COMPLETED"];
-//   try {
-//     const response = await axios.post(
-//       "/api/webhookSuccess",
-//       {
-//         webhookUrl,
-//         events,
-//       },
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     );
-
-//     return null;
-//   } catch (error) {
-//     console.error("Ошибка при создании webhook:", error);
-//   }
-// };
-const key = process.env.REVOLUT_KEY;
-// export const requestToReturnFailStatus = async () => {
-//   const webhookUrl = "https://delaqua.vercel.app/api/returnNot";
-//   const events = ["ORDER_PAYMENT_DECLINED"];
-//   try {
-//     const response = await axios.post(
-//       "/api/webhookFail",
-//       {
-//         webhookUrl,
-//         events,
-//       },
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${key}`,
-//         },
-//       }
-//     );
-
-//     return null;
-//   } catch (error) {
-//     console.error("Ошибка при создании webhook:", error);
-//   }
-// };
-
+const key =
+  "sk_r-rVS2vIlgIfhVpv-Cw1mWazWOY6JypuXTbvx1Cw5EhoxjjA4cZkyXpAwbiTCqxT";
 export const requestGeneral = async () => {
-  const webhookUrl = "https://delaqua.vercel.app/api/returnHook";
+  const webhookUrl = "https://delaqua.vercel.app/api/return";
   const events = [
     "ORDER_COMPLETED",
-    "ORDER_AUTHORISED",
     "ORDER_CANCELLED",
-    "ORDER_PAYMENT_AUTHENTICATED",
     "ORDER_PAYMENT_DECLINED",
     "ORDER_PAYMENT_FAILED",
   ];
