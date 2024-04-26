327ddc1d7070ea7cca3e754ace378284cbff1e02
Illia <illia.horban@gmail.com>
2024-04-26 15:55:53 +0300

    myform

--- app/components/Account/Account.tsx
+++ app/components/Account/Account.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Container } from "@mui/material";
 import styles from "./Account.module.css";
-import MyForm from "../form/page";
+import MyForm from "../MyForm/MyForm";
 import { useRouter } from "next/navigation";
 
 export default function Account() {
