67d2fc41744c9f8c92f2ec0240f542fe93503e99
NataliGru <nataliia.hrushanyk@gmail.com>
2024-09-30 19:26:42 +0300

    Get user addresses in live, fix return botles calculation, add translations

--- app/components/OrderStepper/components/DateStep/index.tsx
+++ app/components/OrderStepper/components/DateStep/index.tsx
@@ -12,6 +12,7 @@ import {
   FormHelperText,
   Radio,
   RadioGroup,
+  Typography,
 } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -30,6 +31,7 @@ import timezone from "dayjs/plugin/timezone";
 import updateLocale from "dayjs/plugin/updateLocale";
 import utc from "dayjs/plugin/utc";
 import { DataStepModal } from "../DataStepModal";
+import { OrderCardCounter } from "../OrderCardCounter";
 
 dayjs.extend(customParseFormat);
 dayjs.extend(updateLocale);
@@ -44,6 +46,7 @@ dayjs.tz.setDefault("Europe/Nicosia");
 interface FormValues {
   deliveryDate: Dayjs | string;
   deliveryTime: string;
+  bottlesNumberToReturn?: string;
 }
 
 export const DateStep = ({
@@ -67,20 +70,26 @@ export const DateStep = ({
   const { isSmallScreen } = useScreenSize();
   const { showErrorToast } = useToast();
 
-  const [showTooltipMessage, setShowTooltipMessage] = useState(true);
   const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const [showTooltipMessage, setShowTooltipMessage] = useState({
+    date: true,
+    bottles: true,
+  });
   const [nextDay, setNextDay] = useState(dayjs());
   const [openModal, setOpenModal] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { control, handleSubmit, watch } = useForm<FormValues>({
+  const { control, handleSubmit, watch, setValue } = useForm<FormValues>({
     defaultValues: {
       deliveryDate: dayjs(userOrder.deliveryDate, "DD.MM.YYYY"),
       deliveryTime: userOrder.deliveryTime,
+      ...(returnBottles && { bottlesNumberToReturn: "0" }),
     },
   });
 
   const selectedDate = watch("deliveryDate");
+  const returnBottlesCount = watch("bottlesNumberToReturn");
 
   const {
     isCurrentDayAfterTen,
@@ -92,7 +101,13 @@ export const DateStep = ({
   } = deliveryValidation(selectedDate as Dayjs, allOrders);
 
   const onSubmit = async (data: FormValues) => {
-    if (showTooltipMessage) return;
+    if (showTooltipMessage.date) {
+      setShowTooltipMessage((prev) => ({
+        ...prev,
+        date: true,
+      }));
+      return;
+    }
 
     const dateObj = {
       deliveryDate: (data.deliveryDate as Dayjs).format("DD.MM.YYYY"),
@@ -104,9 +119,16 @@ export const DateStep = ({
     if (returnBottles) {
       setLoading(true);
 
+      handleAddOrderDetails({
+        bottlesNumberToReturn: data.bottlesNumberToReturn,
+      });
+
       const orderData = {
         ...userOrder,
         ...dateObj,
+        ...(returnBottles && {
+          bottlesNumberToReturn: data.bottlesNumberToReturn,
+        }),
         createdAt: dayjs().format("DD.MM.YYYY HH:mm"),
         items: [],
         paymentMethod: "Return cash",
@@ -140,11 +162,11 @@ export const DateStep = ({
       infoDay ||
       isOrdersLimitReached;
 
-    setShowTooltipMessage(disableNextConditions);
-
     !isCurrentDayPrevious &&
       !isCurrentDayAfterNoon &&
       setOpenModal(isOrdersLimitReached);
+
+    setShowTooltipMessage((prev) => ({ ...prev, date: disableNextConditions }));
   }, [
     isCurrentDayAfterNoon,
     isCurrentDayPrevious,
@@ -169,8 +191,28 @@ export const DateStep = ({
     isCurrentDayAfterNoon,
   ]);
 
+  useEffect(() => {
+    if (returnBottles) {
+      setValue("bottlesNumberToReturn", userOrder.bottlesNumberToReturn);
+    } else {
+      setShowTooltipMessage((prev) => ({
+        ...prev,
+        bottles: false,
+      }));
+    }
+  }, [userOrder, returnBottles]);
+
+  useEffect(() => {
+    if (returnBottles) {
+      setShowTooltipMessage((prev) => ({
+        ...prev,
+        bottles: +(returnBottlesCount || "0") <= 0,
+      }));
+    }
+  }, [returnBottles, returnBottlesCount]);
+
   if (loading) {
-    return <Loader text={t("loading_paymentLink")} />;
+    return <Loader text={t("loading_order")} />;
   }
 
   return (
@@ -207,18 +249,25 @@ export const DateStep = ({
                   shouldDisableDate={(date: Dayjs) => shouldDisableDate(date)}
                   sx={datePickerStyle(isSmallScreen, true)}
                 />
-                <FormHelperText
-                  sx={{ color: errorMessage ? "#d32f2f" : "gray" }}
-                >
-                  {errorMessage || `* ${t("delivery_date")}`}
-                </FormHelperText>
-
+                {
+                  <FormHelperText
+                    sx={{
+                      color: showTooltipMessage.date ? "#d32f2f" : "gray",
+                    }}
+                  >
+                    {!isCurrentDayPrevious && isCurrentDayIsSunday
+                      ? t("sunday")
+                      : isCurrentDayPrevious || isCurrentDayAfterNoon
+                      ? t("change_day")
+                      : `* ${t("delivery_date")}`}
+                  </FormHelperText>
+                }
                 <FormHelperText
                   sx={{
                     color: "#d32f2f",
                   }}
                 >
-                  {showTooltipMessage &&
+                  {showTooltipMessage.date &&
                     t("soonest_day", {
                       nextDay: `${nextDay.format("dddd")} - ${nextDay.format(
                         "DD MMMM"
@@ -243,20 +292,60 @@ export const DateStep = ({
                   value="9-12"
                   control={<Radio />}
                   label={t("delivery_time_9_12")}
-                  disabled={showTooltipMessage || isCurrentDayAfterTen}
+                  disabled={showTooltipMessage.date || isCurrentDayAfterTen}
                 />
                 <FormControlLabel
                   value="9-17"
                   control={<Radio />}
                   label={t("delivery_time_9_17")}
-                  disabled={showTooltipMessage}
+                  disabled={showTooltipMessage.date}
                 />
               </RadioGroup>
             )}
           />
+          {returnBottles && (
+            <Box
+              sx={{
+                marginTop: "30px",
+                marginBottom: "20px",
+              }}
+            >
+              <Controller
+                name="bottlesNumberToReturn"
+                control={control}
+                render={({ field }) => (
+                  <Box display="flex" flexDirection="column">
+                    <Typography textAlign={isSmallScreen ? "center" : "left"}>
+                      {t("number_of_bottles_to_return")}
+                    </Typography>
+
+                    <Box
+                      display="flex"
+                      justifyContent={isSmallScreen ? "center" : "flex-start"}
+                    >
+                      <OrderCardCounter
+                        count={field?.value || "0"}
+                        onAdd={() => field.onChange(+(field?.value || "0") + 1)}
+                        disabled={showTooltipMessage.date}
+                        onRemove={() => {
+                          field.onChange(
+                            Math.max(+(field?.value || "0") - 1, 0)
+                          );
+                        }}
+                      />
+                    </Box>
+                  </Box>
+                )}
+              />
+            </Box>
+          )}
         </Box>
         {renderButtonsGroup(
-          showTooltipMessage ? "Please select correct delivery date" : ""
+          showTooltipMessage.date
+            ? "Please select correct delivery date"
+            : showTooltipMessage.bottles
+            ? "You can not to return 0 bottles"
+            : ""
         )}
       </Box>
       <DataStepModal open={openModal} onClose={() => setOpenModal(false)} />
--- app/components/OrderStepper/components/FinishStep/index.tsx
+++ app/components/OrderStepper/components/FinishStep/index.tsx
@@ -18,6 +18,9 @@ export const FinishStep = ({ returnBottles }: { returnBottles: boolean }) => {
 
   const { userOrder, paymentUrl } = useOrderDetailsContext();
 
+  const count = +userOrder.bottlesNumberToReturn || 0;
+  const countSum = +(userOrder.bottlesNumberToReturn || "0") * 7;
+
   const clickToPay = () => {
     trackAmplitudeEvent("clickToPay", {
       text: "Click ot payment link",
@@ -69,38 +72,78 @@ export const FinishStep = ({ returnBottles }: { returnBottles: boolean }) => {
         marginBottom: "15px",
       }}
     >
-      <Box>
-        <Box className={styles.center}>
-          <CheckBox fontSize="large" className={styles.icon} />
-        </Box>
-
-        <Typography
-          className={styles.center}
-          id="modal-modal-title"
-          variant="h6"
-          component="h2"
-        >
-          <span className={styles.bold}> {t("thanks_for_order")}</span>
-        </Typography>
-
-        {!returnBottles && (
-          <Typography className={styles.center} id="modal-modal-description">
-            {userOrder.paymentMethod === "Cash"
-              ? t("we_will_deliver_to_you", {
-                  date: userOrder.deliveryDate,
-                  time: userOrder.deliveryTime,
-                })
-              : t("we_will_deliver_to_you_pay", {
-                  date: userOrder.deliveryDate,
-                  time: userOrder.deliveryTime,
-                })}
+      {returnBottles && (
+        <Box display="flex" flexDirection="column" gap="30px">
+          <Typography
+            className={styles.center}
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          >
+            <span className={styles.bold}>
+              {userOrder.bottlesNumberToReturn ===
+              userOrder.deliveryAddressObj.numberOfBottles
+                ? t("sorry")
+                : t("accept")}
+            </span>
           </Typography>
-        )}
 
-        <Box className={styles.center} id="modal-modal-description">
-          {paymentText}
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Typography maxWidth="98%">
+              {t("sum", { count, countSum })}
+            </Typography>
+            <Typography>{t("afterSum")}</Typography>
+          </Box>
+
+          {userOrder.bottlesNumberToReturn ===
+            userOrder.deliveryAddressObj.numberOfBottles && (
+            <Typography
+              className={styles.center}
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+            >
+              <span className={styles.bold}>{t("thanks")}</span>
+            </Typography>
+          )}
         </Box>
-      </Box>
+      )}
+
+      {!returnBottles && (
+        <Box>
+          <Box className={styles.center}>
+            <CheckBox fontSize="large" className={styles.icon} />
+          </Box>
+
+          <Typography
+            className={styles.center}
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          >
+            <span className={styles.bold}> {t("thanks_for_order")}</span>
+          </Typography>
+
+          {!returnBottles && (
+            <Typography className={styles.center} id="modal-modal-description">
+              {userOrder.paymentMethod === "Cash"
+                ? t("we_will_deliver_to_you", {
+                    date: userOrder.deliveryDate,
+                    time: userOrder.deliveryTime,
+                  })
+                : t("we_will_deliver_to_you_pay", {
+                    date: userOrder.deliveryDate,
+                    time: userOrder.deliveryTime,
+                  })}
+            </Typography>
+          )}
+
+          <Box className={styles.center} id="modal-modal-description">
+            {paymentText}
+          </Box>
+        </Box>
+      )}
+
       <Button
         variant="contained"
         sx={{
