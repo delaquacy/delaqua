ca0680eaabf32f549ad76ebadfa593ecc070f332
NataliGru <nataliia.hrushanyk@gmail.com>
2024-10-02 19:41:45 +0300

    Add modal window to cancel order

---
+++ app/components/History/CancelModal.tsx
@@ -0,0 +1,175 @@
+import { OrdersData } from "@/app/types";
+import { Close, Report } from "@mui/icons-material";
+import { Box, Button, IconButton, Modal, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import {
+  CancelTitle,
+  CardRow,
+  HistoryCardWrapper,
+  ModalWrapper,
+  TextTypo,
+  TitleTypo,
+} from "./styled";
+
+interface CancelModalProps {
+  open: boolean;
+  handleClose: () => void;
+  orderToCancel: OrdersData | null;
+}
+
+export const CancelModal = ({
+  open,
+  handleClose,
+  orderToCancel,
+}: CancelModalProps) => {
+  const { t } = useTranslation(["orderslist", "orderTable"]);
+
+  const paymentStatusText = Array.isArray(orderToCancel?.paymentStatus)
+    ? orderToCancel.paymentStatus
+        .map((status) =>
+          t(`paymentStatuses.${status.toLowerCase().replace(/\s+/g, "_")}`, {
+            ns: "orderTable",
+          })
+        )
+        .join(", ")
+    : typeof orderToCancel?.paymentStatus === "string"
+    ? t(
+        `paymentStatuses.${orderToCancel?.paymentStatus
+          .toLowerCase()
+          .replace(/\s+/g, "_")}`,
+        {
+          ns: "orderTable",
+        }
+      )
+    : t("paymentStatuses.unpaid");
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <ModalWrapper>
+        <IconButton
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            right: 5,
+            top: 5,
+          }}
+        >
+          <Close />
+        </IconButton>
+        <Box>
+          <Report
+            sx={{
+              width: "40px",
+              height: "40px",
+              color: "#D34942",
+            }}
+          />
+          <CancelTitle>
+            {t("cancelOrder", {
+              ns: "main",
+            })}
+          </CancelTitle>
+
+          <Typography fontSize="18px">
+            {t("sureCancel", {
+              ns: "main",
+            })}
+          </Typography>
+        </Box>
+
+        <HistoryCardWrapper>
+          <CardRow>
+            <TitleTypo>{t("table_phone")}:</TitleTypo>
+            <TextTypo>{orderToCancel?.phoneNumber}</TextTypo>
+          </CardRow>
+
+          <CardRow>
+            <TitleTypo>{t("table_delivery_time")}:</TitleTypo>
+            <TextTypo>
+              {`${orderToCancel?.deliveryDate}, ${orderToCancel?.deliveryTime}`}
+            </TextTypo>
+          </CardRow>
+
+          <Box textAlign="left">
+            <Typography
+              variant="body1"
+              fontWeight="bold"
+              marginRight="5px"
+              display={"inline"}
+              textAlign="left"
+            >
+              {t("table_address")}:
+            </Typography>
+            <Typography
+              variant="body1"
+              display="inline"
+              width="100%"
+              textAlign="left"
+            >
+              {`${orderToCancel?.postalIndex}, ${orderToCancel?.deliveryAddress}`}
+            </Typography>
+          </Box>
+
+          <CardRow>
+            <TitleTypo>{t("table_payment_method")}:</TitleTypo>
+            <TextTypo>{orderToCancel?.paymentMethod}</TextTypo>
+          </CardRow>
+
+          <CardRow>
+            <TitleTypo>{t("table_total")}:</TitleTypo>
+            <TextTypo>{orderToCancel?.totalPayments}</TextTypo>
+          </CardRow>
+
+          <CardRow
+            sx={{
+              justifyContent: "space-between",
+            }}
+          >
+            <CardRow
+              sx={{
+                flex: 1,
+              }}
+            >
+              <TitleTypo>{t("table_status")}:</TitleTypo>
+              <TextTypo>{paymentStatusText}</TextTypo>
+            </CardRow>
+          </CardRow>
+        </HistoryCardWrapper>
+
+        <Box display="flex" flexDirection="row" width="100%" gap="20px">
+          <Button
+            variant="contained"
+            onClick={handleClose}
+            sx={{
+              textTransform: "none",
+              width: "100%",
+              background: "#478547",
+              ":hover": {
+                background: "#356435",
+              },
+            }}
+          >
+            {t("back", {
+              ns: "main",
+            })}
+          </Button>
+
+          <Button
+            variant="contained"
+            onClick={handleClose}
+            sx={{
+              textTransform: "none",
+              width: "100%",
+              background: "#B43636",
+              ":hover": { background: "#7D2525" },
+            }}
+          >
+            {t("confirm", {
+              ns: "main",
+            })}
+          </Button>
+        </Box>
+      </ModalWrapper>
+    </Modal>
+  );
+};
--- app/components/History/index.tsx
+++ app/components/History/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useUserContext } from "@/app/contexts/UserContext";
 import { useScreenSize } from "@/app/hooks";
+import { OrdersData } from "@/app/types";
 import { getDateFromTimestamp } from "@/app/utils";
 import { ApartmentOutlined, HouseOutlined } from "@mui/icons-material";
 import {
   Box,
+  Button,
   CircularProgress,
   Paper,
   Table,
@@ -17,11 +19,14 @@ import {
   Typography,
 } from "@mui/material";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { MainContentWrapper } from "../shared/styled";
+import { CancelModal } from "./CancelModal";
 import { HistoryTableRow } from "./HistoryTableRow";
 import {
   CardRow,
+  HeaderWrapper,
   HistoryCardWrapper,
   HistoryCardsWrapper,
   HistoryTableHeadCell,
@@ -34,9 +39,35 @@ const GeneratePdf = dynamic(() => import("../InvoiceGenerator"), {
 });
 
 export const History = () => {
-  const { t } = useTranslation(["orderslist", "orderTable"]);
+  const { t } = useTranslation([
+    "orderslist",
+    "orderTable",
+    "form",
+    "savedAddresses",
+  ]);
   const { orders, loading } = useUserContext();
   const { isSmallScreen } = useScreenSize();
+  const [hasUncompletedOrder, setHasUncompletedOrder] = useState(false);
+  const [showCancelModal, setShowCancelModal] = useState(false);
+  const [orderToCancel, setOrderToCancel] = useState<null | OrdersData>(null);
+
+  const handleOpenCancelModal = (order: OrdersData) => {
+    setShowCancelModal(true);
+    setOrderToCancel(order);
+  };
+
+  const handleCloseCancelModal = () => {
+    setShowCancelModal(false);
+    setOrderToCancel(null);
+  };
+
+  useEffect(() => {
+    const hasUncompletedOrder = orders.some(
+      (order) => !order.canceled && !order.completed
+    );
+
+    setHasUncompletedOrder(hasUncompletedOrder);
+  }, [orders]);
 
   if (loading) {
     return (
@@ -56,163 +87,200 @@ export const History = () => {
   }
 
   return (
-    <MainContentWrapper>
-      <Typography variant="h4">{t("history")}</Typography>
+    <>
+      <MainContentWrapper>
+        <HeaderWrapper>
+          <Typography variant="h4">{t("history")}</Typography>
+        </HeaderWrapper>
 
-      {isSmallScreen ? (
-        <HistoryCardsWrapper>
-          {orders.map((order, index) => (
-            <HistoryCardWrapper key={order.id + index}>
-              <Tooltip
-                title={`${order.postalIndex}, ${order.deliveryAddress}`}
-                placement="bottom-start"
-              >
-                <CardRow>
-                  {!order?.deliveryAddressObj?.addressType ||
-                  order?.deliveryAddressObj?.addressType === "Home" ? (
-                    <HouseOutlined />
-                  ) : (
-                    <ApartmentOutlined />
-                  )}
-                  <Typography fontSize="14px">
-                    {t(
-                      order?.deliveryAddressObj?.addressType?.toLowerCase() ||
-                        "home",
-                      { ns: "savedAddresses" }
+        {isSmallScreen ? (
+          <HistoryCardsWrapper>
+            {orders.map((order, index) => {
+              const couldBeCanceled =
+                hasUncompletedOrder && !order.canceled && !order.completed;
+
+              return (
+                <HistoryCardWrapper key={order.id + index}>
+                  <Tooltip
+                    title={`${order.postalIndex}, ${order.deliveryAddress}`}
+                    placement="bottom-start"
+                  >
+                    <CardRow>
+                      {!order?.deliveryAddressObj?.addressType ||
+                      order?.deliveryAddressObj?.addressType === "Home" ? (
+                        <HouseOutlined />
+                      ) : (
+                        <ApartmentOutlined />
+                      )}
+                      <Typography fontSize="14px">
+                        {t(
+                          order?.deliveryAddressObj?.addressType?.toLowerCase() ||
+                            "home",
+                          { ns: "savedAddresses" }
+                        )}
+                      </Typography>
+                    </CardRow>
+                  </Tooltip>
+                  <CardRow>
+                    <TitleTypo>{t("table_phone")}:</TitleTypo>
+                    <TextTypo>{order.phoneNumber}</TextTypo>
+                  </CardRow>
+
+                  <CardRow>
+                    <TitleTypo>{t("table_delivery_time")}:</TitleTypo>
+                    <TextTypo>
+                      {`${order.deliveryDate}, ${order.deliveryTime}`}
+                    </TextTypo>
+                  </CardRow>
+
+                  <CardRow>
+                    <TitleTypo>{t("table_order_time")}:</TitleTypo>
+                    <TextTypo>
+                      {typeof order.createdAt === "string"
+                        ? order.createdAt
+                        : getDateFromTimestamp(order.createdAt as any)}
+                    </TextTypo>
+                  </CardRow>
+
+                  <CardRow>
+                    <TitleTypo>{t("table_payment_method")}:</TitleTypo>
+                    <TextTypo>{order.paymentMethod}</TextTypo>
+                  </CardRow>
+
+                  <CardRow>
+                    <TitleTypo>{t("table_total")}:</TitleTypo>
+                    <TextTypo>{order.totalPayments}</TextTypo>
+                  </CardRow>
+
+                  <CardRow
+                    sx={{
+                      flex: 1,
+                    }}
+                  >
+                    <TitleTypo>{t("table_status")}:</TitleTypo>
+                    <TextTypo>
+                      {t(`paymentStatuses.${order.paymentStatus}`, {
+                        ns: "orderTable",
+                      })}
+                    </TextTypo>
+                  </CardRow>
+
+                  <CardRow
+                    sx={{
+                      flex: 1,
+                    }}
+                  >
+                    <TitleTypo>{t("downloadInvoice")}:</TitleTypo>
+                    <GeneratePdf order={order} />
+                  </CardRow>
+
+                  <CardRow>
+                    {couldBeCanceled && (
+                      <Button
+                        sx={{
+                          border: "1px solid",
+                          width: "50%",
+                          textTransform: "capitalize",
+                        }}
+                        onClick={() => handleOpenCancelModal(order)}
+                      >
+                        {t("cancel", {
+                          ns: "form",
+                        })}
+                      </Button>
                     )}
-                  </Typography>
-                </CardRow>
-              </Tooltip>
-              <CardRow>
-                <TitleTypo>{t("table_phone")}:</TitleTypo>
-                <TextTypo>{order.phoneNumber}</TextTypo>
-              </CardRow>
-
-              <CardRow>
-                <TitleTypo>{t("table_delivery_time")}:</TitleTypo>
-                <TextTypo>
-                  {`${order.deliveryDate}, ${order.deliveryTime}`}
-                </TextTypo>
-              </CardRow>
-
-              <CardRow>
-                <TitleTypo>{t("table_order_time")}:</TitleTypo>
-                <TextTypo>
-                  {typeof order.createdAt === "string"
-                    ? order.createdAt
-                    : getDateFromTimestamp(order.createdAt as any)}
-                </TextTypo>
-              </CardRow>
-
-              <CardRow>
-                <TitleTypo>{t("table_payment_method")}:</TitleTypo>
-                <TextTypo>{order.paymentMethod}</TextTypo>
-              </CardRow>
-
-              <CardRow>
-                <TitleTypo>{t("table_total")}:</TitleTypo>
-                <TextTypo>{order.totalPayments}</TextTypo>
-              </CardRow>
-
-              <CardRow
-                sx={{
-                  justifyContent: "space-between",
-                }}
-              >
-                <CardRow
-                  sx={{
-                    flex: 1,
-                  }}
-                >
-                  <TitleTypo>{t("table_status")}:</TitleTypo>
-                  <TextTypo>
-                    {t(`paymentStatuses.${order.paymentStatus}`, {
-                      ns: "orderTable",
-                    })}
-                  </TextTypo>
-                </CardRow>
-
-                <CardRow
-                  sx={{
-                    flex: 1,
-                  }}
-                >
-                  <TitleTypo>{t("downloadInvoice")}:</TitleTypo>
-                  <GeneratePdf order={order} />
-                </CardRow>
-              </CardRow>
-            </HistoryCardWrapper>
-          ))}
-        </HistoryCardsWrapper>
-      ) : (
-        <TableContainer
-          component={Paper}
-          sx={{
-            paddingInline: "20px",
-            paddingBottom: "20px",
-
-            height: isSmallScreen
-              ? `calc(100dvh - 84px - 129px)`
-              : `calc(100dvh - 110px - 129px)`,
-            boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-          }}
-        >
-          <Table
-            stickyHeader
+                  </CardRow>
+                </HistoryCardWrapper>
+              );
+            })}
+          </HistoryCardsWrapper>
+        ) : (
+          <TableContainer
+            component={Paper}
             sx={{
-              paddingBottom: "10px",
-              overflowY: "scroll",
+              paddingInline: "20px",
+              paddingBottom: "20px",
+
+              height: isSmallScreen
+                ? `calc(100dvh - 84px - 129px)`
+                : `calc(100dvh - 70px - 129px)`,
+              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
             }}
           >
-            <TableHead>
-              <TableRow>
-                <HistoryTableHeadCell>{t("table_phone")}</HistoryTableHeadCell>
-                <HistoryTableHeadCell align="center">
-                  {t("table_address")}
-                </HistoryTableHeadCell>
-
-                <HistoryTableHeadCell
-                  sx={{
-                    maxWidth: "120px",
-                  }}
-                >
-                  {t("table_delivery_time")}
-                </HistoryTableHeadCell>
-
-                <HistoryTableHeadCell
-                  sx={{
-                    maxWidth: "120px",
-                  }}
-                >
-                  {t("table_order_time")}
-                </HistoryTableHeadCell>
-
-                <HistoryTableHeadCell>
-                  {t("table_payment_method")}
-                </HistoryTableHeadCell>
-
-                <HistoryTableHeadCell>{t("table_total")}</HistoryTableHeadCell>
-                <HistoryTableHeadCell>{t("table_status")}</HistoryTableHeadCell>
-                <HistoryTableHeadCell>{t("invoice")}</HistoryTableHeadCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {orders.length ? (
-                orders.map((order, index) => (
-                  <HistoryTableRow key={order.id + index} order={order} />
-                ))
-              ) : (
+            <Table
+              stickyHeader
+              sx={{
+                paddingBottom: "10px",
+                overflowY: "scroll",
+              }}
+            >
+              <TableHead>
                 <TableRow>
-                  <TableCell colSpan={12} align="center">
-                    {t("table_no_orders_yet")}
-                  </TableCell>
+                  <HistoryTableHeadCell>
+                    {t("table_phone")}
+                  </HistoryTableHeadCell>
+                  <HistoryTableHeadCell align="center">
+                    {t("table_address")}
+                  </HistoryTableHeadCell>
+
+                  <HistoryTableHeadCell
+                    sx={{
+                      maxWidth: "120px",
+                    }}
+                  >
+                    {t("table_delivery_time")}
+                  </HistoryTableHeadCell>
+
+                  <HistoryTableHeadCell
+                    sx={{
+                      maxWidth: "120px",
+                    }}
+                  >
+                    {t("table_order_time")}
+                  </HistoryTableHeadCell>
+
+                  <HistoryTableHeadCell>
+                    {t("table_payment_method")}
+                  </HistoryTableHeadCell>
+
+                  <HistoryTableHeadCell>
+                    {t("table_total")}
+                  </HistoryTableHeadCell>
+                  <HistoryTableHeadCell>
+                    {t("table_status")}
+                  </HistoryTableHeadCell>
+                  <HistoryTableHeadCell>{t("invoice")}</HistoryTableHeadCell>
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
-    </MainContentWrapper>
+              </TableHead>
+
+              <TableBody>
+                {orders.length ? (
+                  orders.map((order, index) => (
+                    <HistoryTableRow
+                      key={order.id + index}
+                      order={order}
+                      hasUncompletedOrder={hasUncompletedOrder}
+                      handleOpenCancelModal={handleOpenCancelModal}
+                    />
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={12} align="center">
+                      {t("table_no_orders_yet")}
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </MainContentWrapper>
+
+      <CancelModal
+        open={showCancelModal}
+        handleClose={handleCloseCancelModal}
+        orderToCancel={orderToCancel}
+      />
+    </>
   );
 };
