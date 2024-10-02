22bee28feb1e625dd9902e401c753513e3c8fd8b
NataliGru <nataliia.hrushanyk@gmail.com>
2024-10-02 19:41:45 +0300

    Add modal window to cancel order

--- app/components/History/styled.ts
+++ app/components/History/styled.ts
@@ -1,3 +1,4 @@
+import { theme } from "@/app/ui/themeMui";
 import { Box, Card, TableCell, Typography, styled } from "@mui/material";
 
 export const HistoryTableHeadCell = styled(TableCell)({
@@ -17,6 +18,12 @@ export const HistoryTableWidthCell = styled(TableCell)({
   maxWidth: "120px",
   textAlign: "center",
   paddingInline: "5px",
+});
+
+export const HeaderWrapper = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
 });
 
 export const HistoryCardsWrapper = styled(Box)({
@@ -51,4 +58,31 @@ export const TitleTypo = styled(Typography)({
 });
 export const TextTypo = styled(Typography)({
   fontSize: "14px",
+});
+export const CancelTitle = styled(Typography)({
+  fontSize: "28px",
+  fontWeight: "bold",
+});
+
+export const ModalWrapper = styled(Box)({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  width: 350,
+
+  transform: "translate(-50%, -50%)",
+  border: "2px solid #000",
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.shadows[24],
+  padding: "20px",
+  textAlign: "center",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "20px",
+
+  [theme.breakpoints.up("sm")]: {
+    width: 500,
+  },
 });
